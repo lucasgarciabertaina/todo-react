09ba257b864e1d0291d3b2417bf63dddbb302ed1
Lucas García Bertaina <lucasgarciabertaina@gmail.com>
2022-04-11 13:33:24 -0300

    setting up app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './profile.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img className="nav__profile-photo" src={logo} alt="200" />
+        <div className="nav__profile-elements">
+          <h4 className="nav__profile-welcome">Welcome Back</h4>
+          <h3 className="nav__profile-name">Name</h3>
+        </div>
+        <div className="search">
+          <img className="search__icon" alt="300" />
+        </div>
+      </nav>
+      ,
+      <div className="todo-now">
+        <h3 className="todo-now__title">Now</h3>
+        <div className="todo-now__todo">
+          <img className="todo-now__image" alt="300" />
+          <div className="todo-now__activity-type">
+            <h2 className="todo-now__activity">Crossfit</h2>
+            <h4 className="todo-now__type">Training</h4>
+          </div>
+        </div>
+      </div>
+      ,
+      <button className="button" >New Todo</button>
+      ,
+      <div className="todoay-section">
+        <section className="today-section__today-see-all">
+          <h3 className="today-see-all__today">Today</h3>
+          <a className="today-see-all__see-all" href="./index.js">See All</a>
+        </section>,
+        <section className="today-section__todos">
+          <div className="todos__todo">
+            <img className="todo__image" alt="300" />
+            <div className="todo__activity-type">
+              <h4 className="todo__activity">Crossfit</h4>
+              <h5 className="todo__type">Training</h5>
+            </div>
+          </div>
+          <div className="todo__check">
+            <img className="check__image" alt="300" />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
